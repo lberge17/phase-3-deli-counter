35d29bde0dd7ca9269a331ad0f92e16bae9e80ac
Laura Berge <lauraeberge@gmail.com>
2021-07-19 10:09:22 -0500

    commit tree dd0c333e8ea0b94802ba67b483bf6bab21b36c40

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+require "pry"
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts("The line is currently empty.")
+    else
+        i = 1
+        sentence = "The line is currently:"
+        katz_deli.each do |line|
+            sentence += " #{i}. #{line}"
+            i += 1
+        end
+        puts sentence
+    end
+end
